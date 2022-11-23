8ee80fa7e398648437dddc9e793b18e48757ae3e
fernando-daw <flagonovoa@danielcastelao.org>
2022-11-23 20:02:54 +0100

    comentarios para JavaDocs

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
-
+/**
+ * Descripcion
+ * @author Fernando Lago Novoa
+ * @version 23.11.2022
+ */
 public class Main {
     public static void main(String[] args) {
-        perro animal = new perro();
+        Perro animal = new Perro();
         //animal.pelo="corto"; // Al cambiar los atributos de la clase perro a private, en el Main.java no nos deja acceder a dicha clase.
         //animal.color="negro";
         //animal.chip=15;
@@ -21,27 +24,27 @@ public class Main {
 
         //animal.ladrar();
 
-        perro animal_2=new perro();
+        Perro animal_2=new Perro();
         animal_2.setPelo("corto");
         animal_2.setChip(5);
         animal_2.setColor("negro");
         animal_2.setAltura(20);
         animal_2.setPeso(48);
 
-        perro animal_3=new perro();
+        Perro animal_3=new Perro();
         animal_3.setPelo("rizado");
         animal_3.setChip(19);
         animal_3.setColor("blanco");
         animal_3.setAltura(4);
         animal_3.setPeso(3);
 
-        perro [] ladrido_animal = new perro[3];
+        Perro[] ladrido_animal = new Perro[3];
         ladrido_animal [0] = animal;
         ladrido_animal [1] = animal_2;
         ladrido_animal [2] = animal_3;
 
         for (int i=0;i< ladrido_animal.length;i++){
-            System.out.println(perro.ladrar(ladrido_animal[i].getAltura(),ladrido_animal[i].getPeso()));
+            System.out.println(Perro.ladrar(ladrido_animal[i].getAltura(),ladrido_animal[i].getPeso()));
         }
 
 
--- src/Perro.java
+++ src/Perro.java
@@ -1,45 +1,89 @@
-public class perro {
+
+public class Perro {
+   /**
+    * Atributos
+    * @param pelo,color
+    * @param chip,peso,altura.
+    */
    private  String pelo, color;
    private  int chip;
    private int peso,altura;
-
+   /**
+    * Getter.
+    * @return chip
+    */
    public  int getChip() {
       return chip;
    }
-
+   /**
+    * Getter.
+    * @return color
+    */
    public  String getcolor() {
       return color;
    }
-
+   /**
+    * Getter.
+    * @return pelo
+    */
    public  String getPelo() {
       return pelo;
    }
-
+   /**
+    * Getter.
+    * @return altura
+    */
    public int getAltura() {
       return altura;
    }
-
+   /**
+    * Getter.
+    * @return peso
+    */
    public int getPeso() {
       return peso;
    }
-
+   /**
+    * Setter.
+    * @param Chip a definir
+    */
    public void  setChip(int Chip){
       this.chip= chip;
    }
+   /**
+    * Setter.
+    * @param Pelo a definir
+    */
    public void  setPelo(String Pelo){
       this.pelo= pelo;
    }
+   /**
+    * Setter.
+    * @param Color a definir
+    */
    public void  setColor(String Color){
       this.color= color;
    }
+   /**
+    * Setter.
+    * @param altura a definir
+    */
 
    public void setAltura(int altura) {
       this.altura = altura;
    }
-
+   /**
+    * Setter.
+    * @param peso a definir
+    */
    public void setPeso(int peso) {
       this.peso = peso;
    }
+   /**
+    * Metodo para establecer el ladrido dependiendo del perro. (peso y altura)
+    * @param peso,altura.
+    * @return ladrido.
+    */
    public static String ladrar (int peso, int altura) {
       String ladrido="";
       if (peso > 10 && altura > 10) {
